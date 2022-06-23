2ea9b99b431fe3ca3095feb2deba3127dd16909c
Taylor Griffin <tmgriffin73@gmail.com>
2022-06-22 22:50:19 -0400

    remove logs

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Part from './Part';
 
 const Content = ({ parts }) => {
-  console.log(parts);
   return parts.map((part) => {
     return <Part key={part.id} name={part.name} exercises={part.exercises} />;
   });
